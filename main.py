4bd52047ed7906b8db44eba62f95cf3997de20b5
Jacob Thomas <thomajac002@warren.k12.in.us>
2024-10-30 09:22:22 -0400

    inital commit

---
+++ main.py
@@ -0,0 +1,10 @@
+age = int(input("age:"))
+
+if age < 5:
+    print("you should play with toys")
+
+elif age >= 5 and age <= 12:
+    print("you can play outdoor game.")
+
+elif age >= 13:
+    print("you might enjoy video game.")
